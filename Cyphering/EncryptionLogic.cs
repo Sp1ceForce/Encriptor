fe24d406aeb4527b17b63c19d48a7537402a0630
Evgeniy Fedotow <koronazheka@outlook.com>
2021-11-10 11:06:00 +0300

    Закинул необходимые доп проекты

---
+++ Cyphering/EncryptionLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Helpers;
+
+namespace Encryption
+{
+    public class EncryptionLogic
+    {
+        public string EncryptString(string text, string key)
+        {
+            char[] keyChar= key.ToCharArray();
+            char[] textChar = text.ToCharArray();
+            char[] resultChar = new char[keyChar.Length];
+            for (int i = 0; i < keyChar.Length; i++)
+            {
+               resultChar[i] = CypherHelper.MoveLetterForward(textChar[i],int.Parse(keyChar[i].ToString()));
+            }
+            return new string(resultChar);
+        }
+        public string DecipherString(string text, string key)
+        {
+            char[] keyChar = key.ToCharArray();
+            char[] textChar = text.ToCharArray();
+            char[] resultChar = new char[keyChar.Length];
+            for (int i = 0; i < keyChar.Length; i++)
+            {
+                resultChar[i] = CypherHelper.MoveLetterBackward(textChar[i], int.Parse(keyChar[i].ToString()));
+            }
+            return new string(resultChar);
+        }
+    }
+}
---
+++ Helpers/CypherHelp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Helpers
+{
+    public class CypherHelper
+    {
+        static char[] alpabet = new char[] {' ','А','Б', 'В', 'Г', 'Д','Е', 'Ё', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
+            'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+        public static char MoveLetterForward(char originalChar, int moveIndex)
+
+        {
+            
+            int desiredIndex=0;
+            int charIndex = FindLetterIndex(originalChar);
+            if (moveIndex+charIndex > alpabet.Length)
+            {
+                desiredIndex = (charIndex + moveIndex) - alpabet.Length;
+            }
+            else
+            {
+                desiredIndex = charIndex + moveIndex;
+            }
+                return alpabet[desiredIndex];
+        }
+        public static char MoveLetterBackward(char originalChar, int moveIndex)
+        {
+            int desiredIndex = 0;
+            int charIndex = FindLetterIndex(originalChar);
+            if ((charIndex- moveIndex) < 0)
+            {
+                desiredIndex = (charIndex - moveIndex) + alpabet.Length;
+            }
+            else
+            {
+                desiredIndex = charIndex - moveIndex;
+            }
+            return alpabet[desiredIndex];
+        }
+
+        static int FindLetterIndex(char orChar)
+        {
+            for (int i = 0; i < alpabet.Length; i++) { 
+                if (orChar == alpabet[i])
+                {
+                    return i;
+                }
+                else
+                {
+                }
+            }
+           
+            return 0;
+        }
+                  
+    }
+}
+    
+
